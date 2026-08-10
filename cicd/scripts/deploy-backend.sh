528cfac5eae56f9779fd499e4e54c5d2b5ab0701
Artur Sobczyk <alsobczyk@gmail.com>
2026-08-10 23:16:57 +0200

    fix deploy

--- cicd/scripts/deploy-backend.sh
+++ cicd/scripts/deploy-backend.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+cd backend
 sam deploy \
   --stack-name "$SAM_STACK_NAME" \
-  --config-file backend/samconfig.toml \
+  --config-file samconfig.toml \
   --no-confirm-changeset \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
